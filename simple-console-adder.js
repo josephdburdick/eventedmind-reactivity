de681dd6b13d3978b5b53178c2c42546adab2ea7
Joe Burdick <josephdburdick@gmail.com>
2014-12-10 23:23:24 -0500

    Add function that adds from Session variable.

--- simple-console-adder.js
+++ simple-console-adder.js
@@ -1,23 +1,10 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
+  add = function (firstCell, secondCell){
+    var a = Session.get(firstCell) || 0;
+    var b = Session.get(secondCell) || 0;
+    return a + b;
+  }
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+  var result = add('a','b');
+  console.log('result: ', result);
 }
